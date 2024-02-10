1db3b77ed413987e5f69ff6b434b06473ebaf0d2
alexhosp <alexhosp.dev@gmail.com>
2024-02-10 19:27:17 +0100

    feat(UI): added logo icon component

--- components/ui/Button/button.stories.tsx
+++ components/ui/Button/button.stories.tsx
@@ -3,30 +3,26 @@ import { Sun, Moon } from 'lucide-react';
 
 export default {
   component: Button,
-  title: 'Button',
+  title: 'Components/Button',
   tags: ['autodocs'],
 };
 
 export const SunIconButton = {
   args: {
-    variant: 'outline',
+    variant: 'ghost',
     size: 'icon',
     children: (
-      <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+      <Sun className='h-[1.2rem] w-[1.2rem] text-[var(--color-icon)] transition-colors' />
     ),
   },
 };
 
 export const MoonIconButton = {
   args: {
-    variant: 'outline',
+    variant: 'ghost',
     size: 'icon',
     children: (
-      <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+      <Moon className='h-[1.2rem] w-[1.2rem] text-[var(--color-icon)] transition-colors' />
     ),
   },
 };
-
-// variant: outline
-// size: icon
-// children: sun or moon
---
+++ components/ui/assets/LogoIcon/logo-icon.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import { MotionButton } from '@/ui/Button/button';
+import { iconAnimation } from '@/ui/util/animation';
+import { useCallback } from 'react';
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
+
+const LogoIcon: React.FC = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const handleNavigation = useCallback((event: React.MouseEvent) => {
+    event.preventDefault();
+    if (pathname === '/') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      router.push('/');
+    }
+  }, []);
+  return (
+    <MotionButton
+      variant='ghost'
+      size='icon'
+      onClick={handleNavigation}
+      aria-label='Scroll back to top'
+      {...iconAnimation}
+    >
+      <div>
+        <Image
+          src='/logo.png'
+          width={1024}
+          height={1024}
+          alt='Logo Icon'
+          layout='responsive'
+        />
+      </div>
+    </MotionButton>
+  );
+};
+export default LogoIcon;
