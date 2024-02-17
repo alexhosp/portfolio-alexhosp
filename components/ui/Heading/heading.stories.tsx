43d50d06e47ba86d4ac0d7ae67d89fb59d20aded
alexhosp <alexhosp.dev@gmail.com>
2024-02-17 14:33:39 +0100

    feat(UI): added heading UI component variants

--- components/ui/Heading/heading.stories.tsx
+++ components/ui/Heading/heading.stories.tsx
@@ -29,3 +29,129 @@ export const DefaultH1: Story = {
     children: 'Default H1',
   },
 };
+
+export const HeroH1: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h1',
+    size: 'h1Accent',
+    color: 'h1accent',
+    children: 'Accent H1',
+  },
+};
+
+export const DefaultH2: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h2',
+    size: 'h2Default',
+    color: 'default',
+    children: 'Default H2',
+  },
+};
+
+export const BigH2: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h2',
+    size: 'h2Big',
+    color: 'default',
+    children: 'Big H2, only for pages without H1!',
+  },
+};
+
+export const AccentH2Big: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h2',
+    size: 'h2Big',
+    color: 'h2accent',
+    children: 'Big H2, only for pages without H1!',
+  },
+};
+
+export const AccentH2Default: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h2',
+    size: 'h2Default',
+    color: 'h2accent',
+    children: 'Accent H2',
+  },
+};
+
+export const AccentH2Small: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h2',
+    size: 'h2Small',
+    color: 'h2accent',
+    children: 'Small Accent H2',
+  },
+};
+
+export const GradientAccentH2Default: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h2',
+    size: 'h2Default',
+    color: 'h2accentgradient',
+    children: 'Gradient Accent H2',
+  },
+};
+
+export const DefaultH3: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h3',
+    size: 'h3Default',
+    color: 'lighter',
+    children: 'Default H3',
+  },
+};
+
+export const AccentH3: Story = {
+  render: (args) => (
+    <Heading as={args.as} size={args.size} color={args.color}>
+      {args.children}
+    </Heading>
+  ),
+  args: {
+    as: 'h3',
+    size: 'h3Default',
+    color: 'default',
+    children: 'Accent H3',
+  },
+};
--- components/ui/Heading/heading.tsx
+++ components/ui/Heading/heading.tsx
@@ -1,41 +1,45 @@
 import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
+import { motion, MotionProps } from 'framer-motion';
+import { headingAnimations } from '../util/animation';
+import React from 'react';
 
-const headingVariants = cva(
-  ['text-[var(--color-foreground)] font-exo2 text-center'],
-  {
-    variants: {
-      size: {
-        h1Default:
-          'pt-5 pb-0.5 text-[2.5rem] leading-none font-bold tracking-wider md:text-5xl md:pt-11 md:pb-1 lg:text-[4rem] lg:pt-[1.625rem] lg:tracking-[0.09em]',
-        h1Accent: 'text-[6.25rem] font-bold',
-        h2Big: 'text-6xl',
-        h2Default: 'text-4xl',
-        h2Small: 'text-3xl',
-        h3Default: 'text-xl',
-        h3Big: 'text-4xl',
-      },
-
-      color: {
-        default: 'text-[var(--color-foreground)]',
-        h1accent:
-          'bg-clip-text text-transparent bg-gradient-to-r from-[var(--color-accent)] to-[var(--color-accent-active)] shadow-[0_2px_4px_rgba(0,0,0,0.25)]',
-        h2accent: 'text-[var(--color-accent)]',
-        strong: 'text-foreground',
-      },
+const headingVariants = cva([' font-exo2 text-center'], {
+  variants: {
+    size: {
+      h1Default:
+        'mb-4 pt-5 pb-0.5 text-[2.5rem] leading-none font-extrabold tracking-wider md:text-5xl md:pt-11 md:pb-1 lg:text-[4rem] lg:pt-[1.625rem] lg:tracking-[0.09em]',
+      h1Accent: 'text-6xl font-black md:text-7xl lg:text-8xl pb-2',
+      h2Big:
+        'text-[2.5rem] font-extrabold tracking-wider md:text-5xl lg:text-[4rem]',
+      h2Default: 'text-[2rem] font-bold tracking-wide md:text-4xl lg:text-6xl',
+      h2Small: 'text-2xl font-semibold tracking-wider md:text-3xl lg:text-5xl',
+      h3Default:
+        'text-xl uppercase tracking-wider md:text-2xl lg:text-4xl font-semibold',
     },
-    defaultVariants: {
-      size: 'h2Default',
-      color: 'default',
-    },
-  }
-);
 
-interface HeadingProps
+    color: {
+      default: 'text-[var(--color-foreground)]',
+      h1accent:
+        'bg-clip-text text-transparent bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-foreground)] drop-shadow-lg',
+      h2accent: 'text-[var(--color-accent)] drop-shadow-md',
+      h2accentgradient:
+        'bg-clip-text text-transparent bg-gradient-to-r from-[var(--color-accent)] to-[var(--color-accent-active)] drop-shadow-md',
+      lighter: 'text-[var(--color-primary)] drop-shadow-lg',
+    },
+  },
+  defaultVariants: {
+    size: 'h2Default',
+    color: 'default',
+  },
+});
+
+export interface HeadingProps
   extends React.HTMLAttributes<HTMLHeadingElement>,
     VariantProps<typeof headingVariants> {
   as: 'h1' | 'h2' | 'h3';
-  color: 'default' | 'h2accent' | 'h1accent' | 'strong';
+  color: 'default' | 'h2accent' | 'h1accent' | 'lighter' | 'h2accentgradient';
+  children: React.ReactNode;
 }
 
 export const Heading: React.FC<HeadingProps> = ({
@@ -50,4 +54,25 @@ export const Heading: React.FC<HeadingProps> = ({
   const combinedClasses = cn(variantClasses, className);
 
   return <Component className={combinedClasses}>{children}</Component>;
+};
+
+export const MotionHeading: React.FC<HeadingProps & MotionProps> = ({
+  children,
+  as,
+  size,
+  color = 'default',
+  ...props
+}) => {
+  const Component = motion(as);
+  const variantClasses = headingVariants({ size, color });
+  const combinedClasses = cn(variantClasses);
+
+  const selectedAnimation = as === 'h1' ? 'expand' : 'bounce';
+  const animationProps = headingAnimations[selectedAnimation];
+
+  return (
+    <Component className={combinedClasses} {...animationProps} {...props}>
+      {children}
+    </Component>
+  );
 };
