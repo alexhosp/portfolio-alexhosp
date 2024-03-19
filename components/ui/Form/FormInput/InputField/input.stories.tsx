9f540f20dfb49426b728595b7271e3b21d851e95
alexhosp <alexhosp.dev@gmail.com>
2024-03-19 16:49:36 +0000

    fix(unused code): removed unused code files

--- components/ui/Form/FormInput/InputField/input.stories.tsx
+++ components/ui/Form/FormInput/InputField/input.stories.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@/ui/Form/FormInput/InputField/input-v1';
+import { Input } from '@/ui/Form/FormInput/InputField/input';
 import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Input> = {
@@ -12,26 +12,14 @@ type Story = StoryObj<typeof Input>;
 
 export const EmailInputDefault: Story = {
   args: {
-    as: 'input',
     type: 'email',
     placeholder: 'Email',
-  },
-};
-
-export const TextInput: Story = {
-  args: {
-    as: 'textarea',
-    placeholder: 'Message',
-    label: 'Name',
   },
 };
 
 export const InputSmall: Story = {
   args: {
-    as: 'input',
     type: 'email',
     placeholder: 'Email',
-    fieldSize: 'small',
-    label: 'Email',
   },
 };
