6e2396a542a8ffc6da0e45c8ca78fe3f3ad06008
alexhosp <alexhosp.dev@gmail.com>
2024-03-19 22:36:00 +0000

    fix(svg): added min width to svg

--- components/ui/assets/HeroSVG/hero-svg.tsx
+++ components/ui/assets/HeroSVG/hero-svg.tsx
@@ -27,7 +27,7 @@ const pathVariants = {
 
 export const HeroSVG = () => {
   return (
-    <div className='flex justify-center items-center w-full h-full mb-4 lg:max-h-[50vh] min-h-[40vh] min-w-[70vw]'>
+    <div className='flex justify-center items-center w-full h-full mb-4 lg:max-h-[50vh] min-h-[40vh] min-w-[80vw] md:min-w-[0]'>
       <motion.svg
         viewBox='0 0 80 80'
         version='1.1'
