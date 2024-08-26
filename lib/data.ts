e8b6f6972d93e7acf6c05b2889424a8ffa36600c
alexhosp <alexhosp.dev@gmail.com>
2024-08-26 14:43:30 +0200

    feat(home): Add slider component to homepage and add new project

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,5 @@
 import prisma from '@/lib/db';
+import { getPlaiceholder } from 'plaiceholder';
 
 export const getAboutContent = async () => {
   // add logic to fetch the longDescription conditionally
